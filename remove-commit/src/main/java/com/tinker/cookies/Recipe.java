50f571e05b439af591ac298e57141eefcf13aaa2
8love <37396548+8love@users.noreply.github.com>
2019-12-22 05:25:39 -0800

    Changed interfaces to classes

--- remove-commit/src/main/java/com/tinker/cookies/Recipe.java
+++ remove-commit/src/main/java/com/tinker/cookies/Recipe.java
@@ -1,10 +1,18 @@
 package com.tinker.cookies;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Recipe {
 
-public interface Recipe {
-
-    List<Ingredient> getIngredients();
-    List<String> getSteps();
-    List<String> getCookingTools();
+    private List<Ingredient> ingredients;
+    private List<String> steps;
+    public List<String> cookingTools;
 }
