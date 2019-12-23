83da3d2e441daa549d6215775394fbc29da5360a
8love <37396548+8love@users.noreply.github.com>
2019-12-23 03:16:14 -0800

    Prepare to remove unit

---
+++ remove-commit/src/main/java/com/tinker/cooking/Measurement.java
@@ -0,0 +1,15 @@
+package com.tinker.cooking;
+
+public enum Measurement {
+    OZ, GRAM
+
+    // somethin bad
+    // somethin bad
+    // somethin bad
+    // somethin bad
+    // somethin bad
+    // somethin bad
+    // somethin bad
+    // somethin bad
+    // somethin bad
+}
